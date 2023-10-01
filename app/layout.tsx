6e034d365c9d37cb5b5dc90961e035285563ccc1
DefiCody <ambergridpro@gmail.com>
2023-10-01 18:32:47 +0530

    lop

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: any }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey='pk_test_cm9idXN0LXN0dXJnZW9uLTk1LmNsZXJrLmFjY291bnRzLmRldiQ'>
     <html lang="en">
       <head>
         <ColorSchemeScript />
