5a5aa828413c3f05b8c1c8f90f97e6f14e422f86
DefiCody <ambergridpro@gmail.com>
2023-10-01 18:40:53 +0530

    kj

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,8 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-    publicRoutes:["/"]
+    publicRoutes:["/"],
+    secretKey:'sk_test_4WB9F5zfLaJ8u9pKt3yRVlAQEgqoMnSKlz36v3tXG0'
 });
  
 export const config = {
